ef23f0f99755c96094c648b0e7d34b561959b0e2
Donovan <Donovanandrews300@gmail.com>
2019-04-21 21:43:19 -0400

    updated testimonial section

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -16,7 +16,7 @@ const About = ({ location }) => (
         <section className={styles.intro}>
         <h1>About</h1>
         <h3>Who we are:</h3>
-<p>See The Color Consulting LLC, is a diversity and inclusion consulting group specializing in strategic planning, training, public speaking, management, and recruitment. We serve youth development organizations in the US and abroad. STC helps organizations create positive diversity through community partnerships, targeted recruiting, and creative training. Our method is thorough and leaves your organization with sustainable practices to continue to diversify your staff and customer base.</p> 
+<p>See The Color Consulting LLC, is a diversity and inclusion consulting group specializing in strategic planning, training, public speaking, management, and recruitment. We serve youth development organizations in the US and abroad. STC helps organizations create diversity through community partnerships, targeted recruiting, and creative training. Our method is thorough and leaves your organization with sustainable practices to continue to diversify your staff and customer base.</p> 
 
 <h3> Our Mission:</h3>
 <p>We  are committed to working with our clients to develop solutions for their diversity needs. We understand that creating programs that work for many different demographics is difficult. Our team wants to use our many years of experience in the camp and childcare industry to make your team more inclusive to all!</p>
@@ -36,10 +36,10 @@ We look forward to working with you and developing the foundation for diversity 
         </section>
 
       <section>
-        <h2>Testimonital</h2>
+        <h2>Testimonitals</h2>
         <div className={styles.referrals}>
           <div className={styles.referral}>
-            <img src={scottSrc} alt="Lighthouse logo" />
+            <img src={scottSrc} alt="Scott Arizala" />
             <div>
               <h4>Scott Arizala</h4>
               <h5>Founder of The Camp Counselor</h5>
@@ -48,9 +48,9 @@ We look forward to working with you and developing the foundation for diversity 
           </div>
 
           <div className={styles.referral}>
-            <img src={sylviaSrc} alt="Inlight logo" />
+            <img src={sylviaSrc} alt="Sylvia van Meerten" />
             <div>
-              <h4>Silvia van Meerten</h4>
+              <h4>Sylvia van Meerten</h4>
               <h5>Founder of empower autism</h5>
               <p>“”</p>
             </div>
